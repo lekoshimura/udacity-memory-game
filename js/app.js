eafbda2999ada757deb72a793036bbbce03b4a9d
Leonardo Koshimura <leonardo.koshimura@gmail.com>
2018-11-06 13:30:27 +0000

    Shuffled cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,76 @@
-/*
- * Create a list that holds all of your cards
- */
+const cardValues = [
+  'fa-diamond',
+  'fa-paper-plane-o',
+  'fa-anchor',
+  'fa-bolt',
+  'fa-cube',
+  'fa-leaf',
+  'fa-bicycle',
+  'fa-bomb'
+];
 
+class Card {
+  constructor(value) {
+    this.value = value;
+    this.turnedDown = true;
+  }
+  getHTML() {
+    /*
+    <li class="card">
+    <i class="fa fa-diamond"></i>
+    </li>
+    */
+    let li = document.createElement('li');
+    let i = document.createElement('i');
+    li.classList.add('card');
+    // li.classList.add('match');
+    i.classList.add('fa');
+    i.classList.add(this.value);
+    li.appendChild(i);
+    return li;
+  }
+}
+
+class Cards {
+  constructor() {
+    this.items = [];
+  }
+  build() {
+    this.items = [];
+    for (let i = 0; i <= 1; i++) {
+      for (let j = 0; j < cardValues.length; j++)
+        this.items.push(new Card(cardValues[j]));
+    };
+  }
+  shuffle() {
+    shuffle(this.items);
+  }
+}
+
+let game = {
+  reset: function() {
+    this.timeStarted = null;
+    this.timeEnded = null;
+    this.starsRating = 0;
+    this.moves = 0;
+
+    this.cards = new Cards();
+    this.cards.build();
+    this.cards.shuffle();
+  },
+  addCardsToContainer(container) {
+    let fragment = document.createDocumentFragment();
+    for (let i = 0; i < this.cards.items.length; i++) {
+      let cardHTML = this.cards.items[i].getHTML();
+      fragment.appendChild(cardHTML);
+    }
+    container.appendChild(fragment);
+  }
+};
+
+game.reset();
+let container = document.querySelector('.deck');
+game.addCardsToContainer(container);
 
 /*
  * Display the cards on the page
@@ -12,19 +81,19 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
